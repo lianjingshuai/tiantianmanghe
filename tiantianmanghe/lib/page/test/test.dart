58a8d505b23ed8f56043572478d11b6883169102
连京帅 <897032194@qq.com>
2021-08-19 20:43:07 +0800

    测试一下push

---
+++ tiantianmanghe/lib/page/test/test.dart
@@ -0,0 +1,40 @@
+/*
+ * @Description:  
+ * @Author:  
+ * @Date: 2021-08-19 20:42:43
+ */
+
+import 'package:flutter/material.dart';
+
+class SearchCriteriaListPage extends StatefulWidget {
+  @override
+  _Widgets createState() {
+    return _Widgets();
+  }
+}
+
+class _Widgets extends State<SearchCriteriaListPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('标题'),
+      // ),
+      body: Container(
+        color: Colors.black12
+      ),
+    );
+  }
+
+}
